f1f78e45c1139d8c48265b79fdfe58dcca03dbd2
thejoviian <thejoviian@gmail.com>
2021-05-26 14:03:02 +0200

    Added first helper to redirect users.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Home, Browse, Signin, Signup} from './pages'
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+  const user = {};
   return (
     <Router>
+      <IsUserRedirect 
+        exact 
+        path={ROUTES.SIGN_IN}
+        loggedInPath={ROUTES.BROWSE}
+        user={user}
+      >
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect 
+        exact 
+        path={ROUTES.SIGN_UP}
+        loggedInPath={ROUTES.BROWSE}
+        user={user}
+      >
+        <Signup />
+      </IsUserRedirect>  
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>   
-      <Route exact path={ROUTES.SIGN_UP}>
-        <Signup />
-      </Route>  
-      <Route exact path={ROUTES.SIGN_IN}>
-        <Signin />
-      </Route>  
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
       </Route>   
---
+++ src/helpers/routes.js
@@ -0,0 +1,24 @@
+import { Route, Redirect } from 'react-router-dom'
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest}){
+    return (
+        <Route
+            {...rest}
+            render={() => {
+                if(!user){
+                    return children;
+                }
+                if(user) {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: loggedInPath
+                            }}
+                        />
+                    )
+                }
+                return null;
+            }}
+        />
+    )
+}
