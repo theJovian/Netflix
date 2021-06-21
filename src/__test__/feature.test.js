7802b8ef1e76f18cae3b40de39b4e5670b081e9f
thejoviian <thejoviian@gmail.com>
2021-06-21 12:08:56 +0200

    Created tests for form, feature, and card.

---
+++ src/__test__/feature.test.js
@@ -0,0 +1,41 @@
+import { render, cleanup } from '@testing-library/react';
+import { Feature } from '../components';
+
+describe('<Feature />', () => {
+	it('renders the <Feature /> with populated data', () => {
+		const { container, getByText } = render(
+			<Feature>
+				<Feature.Title>Unlimited films, TV programmes and more</Feature.Title>
+				<Feature.Subtitle>Watch anywhere. Cancel at any time</Feature.Subtitle>
+			</Feature>
+		);
+
+		expect(getByText('Unlimited films, TV programmes and more')).toBeTruthy();
+		expect(getByText('Watch anywhere. Cancel at any time')).toBeTruthy();
+		expect(container.firstChild).toMatchSnapshot();
+	});
+
+	it('renders the <Feature /> with just a title', () => {
+		const { container, queryByText, getByText } = render(
+			<Feature>
+				<Feature.Title>Unlimited films, TV programmes and more</Feature.Title>
+			</Feature>
+		);
+
+		expect(getByText('Unlimited films, TV programmes and more')).toBeTruthy();
+		expect(queryByText('Watch anywhere. Cancel at any time')).toBeFalsy();
+		expect(container.firstChild).toMatchSnapshot();
+	});
+
+	it('renders the <Feature /> with just a subtitle', () => {
+		const { container, queryByText, getByText } = render(
+			<Feature>
+				<Feature.Subtitle>Watch anywhere. Cancel at any time</Feature.Subtitle>
+			</Feature>
+		);
+
+		expect(queryByText('Unlimited films, TV programmes and more')).toBeFalsy();
+		expect(getByText('Watch anywhere. Cancel at any time')).toBeTruthy();
+		expect(container.firstChild).toMatchSnapshot();
+	});
+});
